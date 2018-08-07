165a3581f4bdd04cf02c347d85580b19b556ffb3
jiazhaoxin <123456>
2018-08-07 09:53:19 +0800

    # Conflicts: #	dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java

--- dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
+++ dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
@@ -18,7 +18,11 @@ public class ConsumeServiceImpl implements ConsumeService {
 
     @Override
     public String talk(String task) {
+<<<<<<< HEAD
         System.out.println("task = " + task);
+=======
+//        System.out.println("task = " + task);
+>>>>>>> test
         return demoService.sayHello(task);
     }
 }
