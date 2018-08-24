b58e000ad8b225f133e6086c269d86cbf09fa61b
jiazhaoxin <123456>
2018-08-24 11:19:49 +0800

    Revert "test0001"
    This reverts commit 6fa79f7c5570aa05e6a69a9349596e8e33e7e87c.

--- dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
+++ dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
@@ -22,7 +22,6 @@ public class ConsumeServiceImpl implements ConsumeService {
         System.out.println("task = " + task);
         demoService.sayHello(task);
         System.out.println("task = " + task);
-        System.out.println("task = " + task);
         return "";
     }
 }
