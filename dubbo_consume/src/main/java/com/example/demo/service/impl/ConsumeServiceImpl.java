ba230567d2c768c3d70a46d700b75a9a73d5f112
jiazhaoxin <123456>
2018-08-24 13:35:46 +0800

    huanyuan

--- dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
+++ dubbo_consume/src/main/java/com/example/demo/service/impl/ConsumeServiceImpl.java
@@ -18,11 +18,8 @@ public class ConsumeServiceImpl implements ConsumeService {
 
     @Override
     public String talk(String task) {
+       return  demoService.sayHello(task);
 
-        System.out.println("task = " + task);
-        demoService.sayHello(task);
-        System.out.println("task = " + task);
-        System.out.println("jzx");
-        return "";
+
     }
 }
